ae832764cf14d87993bf27fe97565bb1edc43249
George Edeh <gofy6566@gmail.com>
2026-05-04 17:18:02 +0100

    feat: implement api and uploads proxying in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone', // Optimized for Docker deployment
   async rewrites() {
     let apiUrl = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "");
 
